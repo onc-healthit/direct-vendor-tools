57af99405b43fffc7abd073cc9fe0dd80e66246a
Chris Cimaszewski <ccimaszewski@ainq.com>
2015-07-01 15:32:00 -0400

    Added Password Hashes

---
+++ site-login/src/main/java/org/sitenv/directvendortools/web/util/HashException.java
@@ -0,0 +1,33 @@
+package org.sitenv.directvendortools.web.util;
+
+public class HashException extends Exception {
+
+	public HashException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HashException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HashException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HashException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HashException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
